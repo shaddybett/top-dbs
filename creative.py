00748194f538e957952e76eeb0640ce18ce434df
Shaddy <shadybett540@gmail.com>
2023-12-14 02:20:29 +0300

    inserted some data to the database

--- creative.py
+++ creative.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import uuid
 engine = create_engine('sqlite:///creativeDB.db')
 
-Session = sessionmaker
+Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
@@ -13,7 +13,7 @@ def generate_uuid():
     return str (uuid.uuid4())
 class Book(Base):
     __tablename__='books'
-    bookId = Column('bookId',primary_key=True,default=generate_uuid)
+    bookId = Column('bookId',String,primary_key=True,default=generate_uuid)
     bookName = Column('bookName',String)
     bookPages = Column('bookPages',Integer)
     bookAuthor = Column('bookAuthor',String)
@@ -24,9 +24,16 @@ class Book(Base):
         self.bookPages = bookPages
 
     def add_book(bookName=bookName,bookAuthor=bookAuthor,bookPages=bookPages):
-        exists = 
-
-
-
+        exists = session.query(Book).filter(Book.bookName == bookName).all()
+        if len(exists) > 0:
+            print('Name already exists')
+        else:
+            new_Book = Book(bookName=bookName,bookPages=bookPages,bookAuthor=bookAuthor)
+            session.add(new_Book)
+            session.commit()
+            print('New book added')    
+            
+new_Book = Book(bookName='The Art Of Coding',bookPages=400,bookAuthor='Kipkorir')
+new_Book.add_book
 
 
