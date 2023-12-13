cbd41e4990db740af2fad11dcd23a550201e43c7
Shaddy <shadybett540@gmail.com>
2023-12-14 01:20:34 +0300

    imported session maker

--- creative.py
+++ creative.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,String,Column,Integer
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 # engine = create_engine('sqlite:///creativeDB.db')
 
