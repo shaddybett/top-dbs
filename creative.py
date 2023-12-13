553713880779b7389aa48c8f4c79ae533d9f9c41
Shaddy <shadybett540@gmail.com>
2023-12-14 01:20:49 +0300

    made the database

--- creative.py
+++ creative.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,String,Column,Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# engine = create_engine('sqlite:///creativeDB.db')
+engine = create_engine('sqlite:///creativeDB.db')
 
 # Base = declarative_base()
 
