6c80183a397e7e4c950feaeb3d7960e025ce1d57
Shaddy <shadybett540@gmail.com>
2023-12-14 01:20:22 +0300

    imported declarative base

--- creative.py
+++ creative.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine,String,Column,Integer
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import sessionmaker
 
 # engine = create_engine('sqlite:///creativeDB.db')
