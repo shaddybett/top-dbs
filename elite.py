f3282623d941c44060d2e693e5095ade5f54dd04
Shaddy <shadybett540@gmail.com>
2023-12-14 09:08:11 +0300

    creating database

--- elite.py
+++ elite.py
@@ -1,3 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# after the imports make a database
