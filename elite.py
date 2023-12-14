ea0387c233d96f2e2ee6262abeb937fcac50d670
Shaddy <shadybett540@gmail.com>
2023-12-14 09:18:16 +0300

    imported uuid

--- elite.py
+++ elite.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import uuid
 # after the imports make a database
 db = 'sqlite:///eliteDB.db'
 
