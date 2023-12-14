0e7de51670414b2bd42781b157aec286c5459c24
Shaddy <shadybett540@gmail.com>
2023-12-14 20:46:31 +0300

    imported uuid

--- coder.py
+++ coder.py
@@ -44,7 +44,7 @@
 from sqlalchemy import create_engine,String,Integer,Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# import uuid
+import uuid
 
 # def generate_uuid():
 #     return str(uuid.uuid4()) 
