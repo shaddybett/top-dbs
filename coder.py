2face9098578019d288d30a0a5b9f87a31524a49
Shaddy <shadybett540@gmail.com>
2023-12-14 20:46:09 +0300

    imported declarative base

--- coder.py
+++ coder.py
@@ -42,7 +42,7 @@
                 
 
 from sqlalchemy import create_engine,String,Integer,Column
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import sessionmaker
 # import uuid
 
