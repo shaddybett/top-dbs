2ff23584d0ce2d79c49578ec6c2edaccdd85c859
Shaddy <shadybett540@gmail.com>
2023-12-14 23:55:37 +0300

    created a class Movie

--- top.py
+++ top.py
@@ -6,3 +6,6 @@ import uuid
 def generate_uuid():
     return str(uuid.uuid4)
 Base = declarative_base()
+class Movie(Base):
+    __tablename__='movies'
+    moviedId = Column('movieId',primary_key=True,)
