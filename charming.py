0a7c5a53a565238db63b516fdfe77a08c83ef77f
Shaddy <shadybett540@gmail.com>
2023-12-14 14:16:54 +0300

    made a databse

--- charming.py
+++ charming.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine,String,column,Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# import uuid
+import uuid
+
+db = 'sqlite:///charmingDB.db'
