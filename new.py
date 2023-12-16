8a5b146cf2998ee93c1bac582231fa109ea6f721
Shaddy <shadybett540@gmail.com>
2023-12-16 09:26:22 +0300

    imported uuid

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine,String,Column,Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# import uuid
+import uuid
