99a63a29b08eeaa7b4191b3af39e224a11e566dd
Shaddy <shadybett540@gmail.com>
2023-12-16 09:27:04 +0300

    made a function to generate uuid

--- new.py
+++ new.py
@@ -2,3 +2,6 @@ from sqlalchemy import create_engine,String,Column,Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
+
+def generate_uuid():
+    return str(uuid.uuid4())
